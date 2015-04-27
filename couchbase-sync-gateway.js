0417fb278edbe5aae66dbb6cfaf3c0f262eea683
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-04-26 22:34:41 -0400

    Update sync function

--- couchbase-sync-gateway.js
+++ couchbase-sync-gateway.js
@@ -5,37 +5,42 @@ function (doc, oldDoc) {
 		return;
 	}
 	
-	// must create as and can never change creator
+	// must create as and can never change author
 	if (!oldDoc) {
-		if (!doc.creator) {
-			throw(forbidden: "Must have creator");
+		if (!doc.author) {
+			throw({forbidden: "Must have author"});
 		}
-		requireUser(doc.creator);
+		requireUser(doc.author);
 	}
-	else if (doc.creator != oldDoc.creator) {
-		throw(forbidden: "Cannot change creator");
+	else if (doc.author != oldDoc.author) {
+		throw({forbidden: "Cannot change author"});
 	}
 	
 	// main documents, only to be edited by admins
-	if ("main" == doc.type || "media" == doc.type || "translation" == doc.type) {
+	if ("main" == doc.type || "media" == doc.type || "category" == doc.type) {
 		requireRole("admin");
+		if ("main" == doc.type) {
+			if (!doc.key || !doc.localizations || !doc.tags || 0 == doc.tags.length) {
+				throw({forbidden: "Invalid main document, must have key, localizations and tags"})
+			}
+		}
 		channel("public");
 	}
 	
-	// suggestion document, everybody can create these and has access to the ones created by himself
+	// suggestion document, everybody can create these and has access to the ones created by themselves
 	else if ("suggestion" == doc.type) {
-		var sugg_channel = "suggestion-" + doc.creator;
+		var sugg_channel = "suggestion-" + doc.author;
 		if (!oldDoc) {
-			access(doc.creator, sugg_channel);
-			channel(["admin", sugg_channel);
+			access(doc.author, sugg_channel);
+			channel("admin", sugg_channel);
 		}
 		else {
-			requireAccess(["admin", sugg_channel])
+			requireAccess(["admin", sugg_channel]);
 		}
 	}
 	
 	// unknown document type
 	else {
-		throw(forbidden: "Document type "+doc.type+" not permitted")
+		throw({forbidden: "Document type " + doc.type + " not permitted"});
 	}
 }
